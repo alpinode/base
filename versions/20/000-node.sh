8804c274b071552a8366258f1cfd55ce0c62a65e
Slee Woo <mail@sleewoo.com>
2024-01-13 20:35:59 +0200

    upgraded v20

--- versions/20/000-node.sh
+++ versions/20/000-node.sh
@@ -1,9 +1,9 @@
 
 set -e
 
-apk add --no-cache \
-  nodejs-current=~20.8 \
-  npm=~9.6 \
-  npm-bash-completion=~9 \
+apk add --update --no-cache \
+  nodejs=~20.10 \
+  npm=~10.2 \
+  npm-bash-completion=~10.2 \
 ;
 
